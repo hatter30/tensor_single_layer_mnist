85cbeed63f15ef299579090356b7554287f9fbfe
oh june <tic.for.tac@gmail.com>
2016-11-26 23:31:57 +0900

    add save, load

--- tensor_single_layer.py
+++ tensor_single_layer.py
@@ -16,28 +16,32 @@ y = tf.nn.softmax(tf.matmul(x,w) + b)
 y_ = tf.placeholder("float",[None,10])
 
 cross_entropy = -tf.reduce_sum(y_*tf.log(y))
-
 train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
-sess = tf.Session()
+cross_prediction =tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(cross_prediction,"float"))
 
-sess.run(tf.initialize_all_variables())
+sess = tf.Session()
+saver = tf.train.Saver()
+
+
+#sess.run(tf.initialize_all_variables())
+saver.restore(sess,'checkpoint/tmp1.ckpt')
 
 accuracy_history = []
-
-for i in range(1000):
+for i in range(300):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={x:batch_xs,y_:batch_ys})
 
-    cross_prediction =tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-    accuracy = tf.reduce_mean(tf.cast(cross_prediction,"float"))
+
     accuracy_temp = sess.run(accuracy, feed_dict={x:mnist.test.images,y_:mnist.test.labels})
     print accuracy_temp
     accuracy_history.append(accuracy_temp)
 
+#save_path = saver.save(sess, 'checkpoint/tmp1.ckpt')
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.plot(accuracy_history)
-plt.legend()
 fig.savefig('accuracy.png')
 
