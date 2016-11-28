6be4492c2c19c251033ee3651c658402636f61a6
june5 <june5@june5-pc2.korea.ncsoft.corp>
2016-11-28 11:04:53 +0900

    add logging function

--- tensor_single_layer.py
+++ tensor_single_layer.py
@@ -6,6 +6,17 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 
+import time
+from tqdm import tqdm
+
+######### setting ################ 
+bLoad = False
+bSave = False
+
+trainEpoch = 500
+evalEpoch = 5000
+#################################
+
 w = tf.Variable(tf.zeros([784,10]))
 b = tf.Variable(tf.zeros([10]))
 
@@ -24,24 +35,42 @@ accuracy = tf.reduce_mean(tf.cast(cross_prediction,"float"))
 sess = tf.Session()
 saver = tf.train.Saver()
 
+sess.run(tf.initialize_all_variables())
 
-#sess.run(tf.initialize_all_variables())
-saver.restore(sess,'checkpoint/tmp1.ckpt')
+if bLoad == True:
+	saver.restore(sess,'checkpoint/tmp1.ckpt')
 
+############### accuracy check ####################
+print "Trainig Start !!!"
 accuracy_history = []
-for i in range(300):
+for i in tqdm(range(trainEpoch)):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={x:batch_xs,y_:batch_ys})
-
-
-    accuracy_temp = sess.run(accuracy, feed_dict={x:mnist.test.images,y_:mnist.test.labels})
-    print accuracy_temp
-    accuracy_history.append(accuracy_temp)
-
-#save_path = saver.save(sess, 'checkpoint/tmp1.ckpt')
+	
+    current_accuracy = sess.run(accuracy, feed_dict={x:mnist.test.images,y_:mnist.test.labels})
+    accuracy_history.append(current_accuracy)
+	
+print "%3dth epoch %.2f accuracy\n" % (i+1, current_accuracy )
+	
+if bSave == True:
+	save_path = saver.save(sess, 'checkpoint/tmp1.ckpt')
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.plot(accuracy_history)
 fig.savefig('accuracy.png')
+
+
+
+################ execution time check ################
+print "Evalution Start !!!"
+start_time = time.time()
+for i in tqdm(range(evalEpoch)):
+    current_accuracy = sess.run(accuracy, feed_dict={x:mnist.test.images,y_:mnist.test.labels})
+processing_time = float(time.time() - start_time)
+print "average processing time : %f second" % (processing_time / (evalEpoch+1))
+
+	
+
+
 
